0482d2506d5cc49b5ebbec4c048579e86fe5f589
Chris Paul <cpjeeves@gmail.com>
2026-08-06 01:24:06 -0500

    feat: activate HDMI only for live viewers

--- server/service/stream/mjpeg/mjpeg.go
+++ server/service/stream/mjpeg/mjpeg.go
@@ -1,6 +1,7 @@
 package mjpeg
 
 import (
+	"NanoKVM-Server/service/viewer"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +17,8 @@ type LatestFrame struct {
 }
 
 func Connect(c *gin.Context) {
+	releaseViewer := viewer.Acquire()
+	defer releaseViewer()
 	c.Header("Content-Type", "multipart/x-mixed-replace; boundary=frame")
 	c.Header("Cache-Control", "no-cache")
 	c.Header("Connection", "keep-alive")
